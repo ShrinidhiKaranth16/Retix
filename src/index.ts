37b51eb488df2776341edcb2e9f54a0269dc1bb8
shrinidhi Karanth <shrinidhiskaranth@gmail.com>
2025-10-12 14:27:26 +0530

    additional config

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+interface Color{
+    name:string;
+    code:string;
+}
+
+const colors:Color[] = [
+    {name:"red",code:"#FF0000"},
+    {name:"green",code:"#00FF00"},
+    {name:"blue",code:"#0000FF"},
+    {name:"yellow",code:"#FFFF00"}
+];
+
+export default colors;
