da68afffa7505b6ffb0597ac9972ee5949328dd4
shrinidhi Karanth <shrinidhiskaranth@gmail.com>
2025-10-20 17:34:10 +0530

    'update'

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,4 @@ export * from "./events/base-publisher.js";
 export * from "./events/subjects.js";
 export * from "./events/ticket-created-event.js";
 export * from "./events/ticket-updated-event.js";
+export * from "./events/types/order-status.js";
